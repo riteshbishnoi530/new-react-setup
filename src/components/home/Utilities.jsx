287400ac96435bb0666fa22a9913e950a1e0163a
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-12 15:23:09 +0530

    add margin

--- src/components/home/Utilities.jsx
+++ src/components/home/Utilities.jsx
@@ -5,13 +5,13 @@ import Heading from '../../common/Heading'
 
 function Utilities() {
   return (
-    <div className='px-4 pt-[162px]'>
+    <div className='px-4 pt-[162px] max-lg:pt-16 max-md:pt-12 max-sm:pt-10'>
       <div className='container'>
         <div className='text-center'>
       <Heading heading="Why Choose Us for" spanHeading="Utilities" />
       </div>
         <p className='opacity-90 mt-4 max-md:mt-2 max-lg:text-center max-w-[620px] mx-auto text-center'>This emphasizes both the action and the benefits, drawing in potential customers with a clear value proposition. Let me know if you'd like any adjustments!</p>
-        <div className='flex max-xl:flex-col items-center gap-6 mt-14'>
+        <div className='flex max-xl:flex-col items-center gap-6 mt-14 max-lg:mt-12 max-md:mt-10 max-sm:mt-8'>
           <div className='bg-PrussianBlue max-w-[424px] max-xl:max-w-[692px] p-10 max-sm:p-5 rounded-md'>
             {REVIEW.map((item, index) => (
               <div key={index}>
